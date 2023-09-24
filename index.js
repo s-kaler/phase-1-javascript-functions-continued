bef9c83cd39ea39cf5a8926d398f30e2c145f466
Sundeep <sundeep.kaler@gmail.com>
2023-09-23 23:54:24 -0400

    lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+//let activity = 'roller-skate';
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(flair){
+    return function(descriptor){
+        return `You are ${flair}${descriptor}${flair}!`;
+    }
+}
